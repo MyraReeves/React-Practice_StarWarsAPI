eaacc6894ec2c2863e7a8dca017326b813690ef8
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-20 02:06:52 -0800

    Adds dynamic paths to characterInfo pages

--- StarWarsCharacters/src/main.jsx
+++ StarWarsCharacters/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Header from './Header.jsx'
+import CharacterInfo from './CharacterInfo.jsx'
 import PageNotFound from './PageNotFound.jsx'
+import Header from './Header.jsx'
 
 const router = createBrowserRouter(
   [
     {path:"/", element: <App/>},
+    {path:"/:characterName", element: <CharacterInfo/>},
     {path:"*", element: <PageNotFound />}
   ]
 )
