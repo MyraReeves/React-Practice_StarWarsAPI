84b5bac6cffffa098e338693ff65dfbb75baec25
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-20 03:24:09 -0800

    For easier API calls changes characterName url param to instead be a number

--- StarWarsCharacters/src/CharacterInfo.jsx
+++ StarWarsCharacters/src/CharacterInfo.jsx
@@ -1,16 +1,17 @@
 import {Link, useParams} from "react-router-dom";
+import StarWarsApi from "./StarWarsApi";
 
 function CharacterInfo() {
 
-    const { characterName } = useParams();
-    const image = `https://github.com/MyraReeves/UW_JS-320_Front-End_Application_Development_With_Javascript/blob/main/Assignment4/Star-Wars-Characters/src/images/${characterName}.png?raw=true`;
-    const altImage = `Larger image of ${characterName}`
+    const { number } = useParams();
+    const image = `https://github.com/MyraReeves/React-Practice_StarWarsAPI/blob/main/StarWarsCharacters/src/images/${number}.png?raw=true`;
+    // const altImage = `Larger image of ${characterName}`
 
     return (
         <>
             <div className='character-info'>
-                <img src={image} alt= {altImage} />
-                {/* <StarWarsApi characterNumber = '14' /> */}
+                <img src={image} alt= 'The same image of the character you selected' />
+                <StarWarsApi characterNumber = { number } />
                 <br/><Link to='/'>	🏠︎ Return to Character Index 🏠︎</Link>
             </div>
 
