c1fe0495053ff7038fcd288b451eb48bad222899
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-20 00:50:05 -0800

    Creates header on all pages

---
+++ StarWarsCharacters/src/Header.jsx
@@ -0,0 +1,15 @@
+import logo from './images/header-title1.jpg'
+import headerImage from './images/header-image.jpg'
+import title from './images/header-title2.png'
+
+function Header() {
+
+    return (
+    <header>
+        <img src={logo} alt="Star Wars logo"/><img src={headerImage} alt="header image"/>  <img src={title} alt='The words "Character Info"' className='title'/>
+    </header>
+    )
+  }
+  
+  export default Header
+  
